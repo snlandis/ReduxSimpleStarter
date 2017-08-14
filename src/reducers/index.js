eb56e56508ecdb920d298559e0c2aabcc3af3e7d
Stephen Landis <stephen.n.landis@gmail.com>
2017-08-14 17:38:30 -0500

    inital changes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,12 @@
-import { combineReducers } from 'redux';
+//this is the original code
+// import { combineReducers } from 'redux';
+//
+// const rootReducer = combineReducers({
+//   state: (state = {}) => state
+// });
+//
+// export default rootReducer;
 
-const rootReducer = combineReducers({
-  state: (state = {}) => state
-});
 
-export default rootReducer;
+//we want to create a new component. this component should produce html.
+//so take this component's HTML and put it on the page (in the DOM)
