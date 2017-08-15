7e97ca2df3f76fcb32d77c9eaeebb555980959e8
Stephen Landis <stephen.n.landis@gmail.com>
2017-08-15 14:15:05 -0500

    was in wrong file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+//original code
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+//
+// import App from './components/app';
+// import reducers from './reducers';
+//
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+//
+// ReactDOM.render(
+//   <Provider store={createStoreWithMiddleware(reducers)}>
+//     <App />
+//   </Provider>
+//   , document.querySelector('.container'));
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//const means "this is the final value - it won't change" - App won't change down the line
+const App = function () {
+  return <div>Hi!</div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//1.we want to create a new component. this component should produce html.
+//2.so take this component's HTML and put it on the page (in the DOM)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,7 @@
-//this is the original code
-// import { combineReducers } from 'redux';
-//
-// const rootReducer = combineReducers({
-//   state: (state = {}) => state
-// });
-//
-// export default rootReducer;
+import { combineReducers } from 'redux';
 
+const rootReducer = combineReducers({
+  state: (state = {}) => state
+});
 
-//we want to create a new component. this component should produce html.
-//so take this component's HTML and put it on the page (in the DOM)
+export default rootReducer;
