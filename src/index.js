889e97a8dbcaca2bf611936941de14a3e4b899ff
Stephen Landis <stephen.n.landis@gmail.com>
2017-08-29 16:35:10 -0500

    ytsearch bug

--- src/index.js
+++ src/index.js
@@ -20,10 +20,15 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar'; //pulls in the SearchBar component. No need to include .js so long as it is a .js file
 
 const API_KEY = 'AIzaSyCwbRmrRhqp0my7FoeKwCAfmbUKjN-D6Us';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 //const means "this is the final value - it won't change" - App won't change down the line
 //1.we want to create a new component. this component should produce html.
